956c696611db45f2d84d3599cee02acd0a43c7df
osiris404 <adkha1ed@icloud.com>
2024-12-17 01:43:20 +0200

    init backend code

---
+++ script.js
@@ -0,0 +1,9 @@
+const recordForm = document.getElementById('record-form');
+const nameInput = document.getElementById('name');
+const ageInput = document.getElementById('age');
+const emailInput = document.getElementById('email');
+const recordList = document.getElementById('record-list');
+const editIndexInput = document.getElementById('edit-index');
+const searchInput = document.getElementById('search');
+let records = [];
+
